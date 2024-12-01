f532a36ef2120ea805b9be2ea6e37d5ba2a2f568
Ye Lin Aung <me@yelinaung.com>
2024-12-01 15:59:16 +0800

    add link

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -2,7 +2,7 @@ import sys
 from collections import Counter
 
 """
-To find the total distance between the left list and the right list, add up the distances between all of the pairs you found. In the example above, this is 2 + 1 + 0 + 1 + 2 + 5, a total distance of 11!
+https://adventofcode.com/2024/day/1
 """
 
 with open(sys.argv[1]) as f:
