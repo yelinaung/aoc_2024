53931decfe51c4d8771d4f3ab1988d9f13919215
Ye Lin Aung <me@yelinaung.com>
2024-12-01 15:57:19 +0800

    some fix

---
+++ README.md
@@ -0,0 +1,1 @@
+[![pre-commit.ci status](https://results.pre-commit.ci/badge/github/yelinaung/aoc_2024/master.svg)](https://results.pre-commit.ci/latest/github/yelinaung/aoc_2024/master)
--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 
 """
 To find the total distance between the left list and the right list, add up the distances between all of the pairs you found. In the example above, this is 2 + 1 + 0 + 1 + 2 + 5, a total distance of 11!
@@ -10,9 +11,8 @@ with open(sys.argv[1]) as f:
 # Part 1
 left, right = zip(*data)
 left, right = sorted(left), sorted(right)
-print(sum([abs(l - r) for l, r in zip(left, right)]))
+print(sum([abs(L - R) for L, R in zip(left, right)]))
 
 # Part 2
-from collections import Counter
 right = Counter(right)
-print(sum([l * right.get(l, 0) for l in left]))
+print(sum([L * right.get(L, 0) for L in left]))
