132597ef80b7a7d1cc0b1283eba18b5c669b1bb6
Ye Lin Aung <me@yelinaung.com>
2024-12-04 17:36:29 +0800

    day 3 part 2

--- day_03/day_03.py
+++ day_03/day_03.py
@@ -1,7 +1,7 @@
 import re
 
-filter_re = r"mul\(\d+,\d+\)"
 mul_re = re.compile(r"mul\((\d+),\s*(\d+)\)")
+control_re = re.compile(r"don\'t\(\)|do\(\)")
 
 
 def part_1(file_name: str) -> int:
@@ -14,6 +14,38 @@ def part_1(file_name: str) -> int:
     return total
 
 
+def part_2(file_name: str) -> int:
+    total = 0
+    line = None
+    with open(file_name) as f:
+        line = " ".join(f.readlines())
+
+    evaluating = True
+    last_end = 0
+    for part in control_re.finditer(line):
+        if evaluating:
+            for mul_match in mul_re.finditer(line[last_end : part.start()]):
+                x, y = map(int, mul_match.groups())
+                total += x * y
+        if part.group() == "don't()":
+            evaluating = False
+        elif part.group() == "do()":
+            evaluating = True
+        last_end = part.end()
+
+    if evaluating:
+        for mul_match in mul_re.finditer(line[last_end:]):
+            x, y = map(int, mul_match.groups())
+            total += x * y
+
+    return total
+
+
 def test_part_1():
     assert part_1("day_03/sample.txt") == 161
-    print(part_1("day_03/data.txt"))
+    print(f'\nPart 1 {part_1("day_03/data.txt")}')
+
+
+def test_part_2():
+    assert part_2("day_03/sample2.txt") == 48
+    print(f'\nPart 2 {part_2("day_03/data.txt")}')
